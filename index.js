e3603580de8fd7c5e96b8c441cb60bd77d39657e
Ioan Scurtu <ioan.scurtu120@gmail.com>
2019-09-09 12:42:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = {};
+  Object.assign(newObj,object,{[key]:value})
+  return newObj
+  
+};
+
+function destructivelyUpdateObjectsWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+  
+};
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object)
+  delete newObj[key]
+  return newObj
+
+};
+
+function destructivelyDeleteFromObjectByKey(objest, key){
+  delete object[key]
+  return object
+};
